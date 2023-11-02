2f26748d087e6428cede057038a2753e5b8777f7
Hasan Zahid Afzal <Hasan Zahid Afzal@LAPTOP-3ING1GHL>
2023-11-02 16:45:47 +0500

    Add project files.

---
+++ Project23/Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string opt, bookname;
+    int nday, fine;
+    cout << "Do you wanna borrow or return the book? chose (B or R)" << endl;
+    cin >> opt;
+
+    if (opt == "B")
+    {
+        cout << "Which book do you wanna issue? \n Enter your book name without space: ";
+        cin >> bookname;
+
+        cout << bookname << "\n issued! congrats" << endl ;
+    }
+    else if (opt == "R")
+    {
+        cout << "Which book do you wanna return? \n Enter your book name without space: ";
+        cin >> bookname;
+        cout << "How many days ago did you issue the book?" <<endl;
+        cin >> nday;
+        if (nday > 7)
+        {
+            fine = (nday - 7) * 1;
+            cout << " fine in dollars = " << fine << endl;
+
+        }
+        else
+        {
+            cout << " no fine, good job on returning on time";
+        }
+        cout << bookname << " returned! congrats youre free now ";
+    }
+    system("pause");
+}
+
+	
